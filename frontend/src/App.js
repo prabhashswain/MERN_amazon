483b202cea2a0b74c5226ddd9109376817684fff
prabhashswain <prabhashswain15@gmail.com>
2022-04-30 17:05:43 +0530

    bootstarp added and reducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,36 @@
-import './index.css';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-     <header>
-       <a href='/'>amazon</a>
-     </header>
-     <main>
-       <Routes>
-         <Route path='/' element={<HomeScreen />} />
-         <Route path='/products/:slug' element={<ProductScreen />} />
-       </Routes>
-     </main>
+      <div className="d-flex flex-column site-container">
+      <header>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>amazon</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
+      </header>
+      <main>
+        <Container>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/products/:slug" element={<ProductScreen />} />
+        </Routes>
+        </Container>
+      </main>
+      <footer>
+        <div className="text-center">All rights reserved</div>
+      </footer>
+      </div>
     </BrowserRouter>
   );
 }
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,69 @@
-import React from 'react'
-import data from '../data';
+import React, { useReducer, useEffect } from "react";
+import axios from "axios";
+import logger from 'use-reducer-logger';
+import { Link } from 'react-router-dom';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+
+    case "FETCH_SUCCESS":
+      return { ...state,products:action.payload, loading: false };
+
+    case "FETCH_FAILED":
+      return { ...state, error:action.payload, loading: false };
+
+    default:
+      return state;
+  }
+};
 
 const HomeScreen = () => {
+  const [{ loading,error,products }, dispatch] = useReducer(logger(reducer),{
+    products:[],
+    error:'',
+    loading:true
+  })
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type:"FETCH_REQUEST" })
+      try {
+      const { data } = await axios.get(`/api/products`);
+        dispatch({ type:"FETCH_SUCCESS",payload:data.products })
+      } catch (error) {
+        dispatch({ type:"FETCH_FAILED",payload:error.message })
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <>
-    <h1>Featured Products</h1>
-      <div className='products'>
-        {
-          data.products.map(product=>(
-            <div className='product' key={product.slug}>
-             <a href={`/products/${product.slug}`}>
-             <img src={product.image} alt={product.name} />
-             </a>
-              <div className='product-info'>
-              <a href={`/products/${product.slug}`}>
-              <p>{product.name}</p>
-              </a>
-              <p><strong>$</strong> {product.price}</p>
+      <h1>Featured Products</h1>
+      <div className="products">
+        { loading ? <div>Loading...</div>
+        : error ? <div>error</div>
+        :
+        products.map((product) => (
+          <div className="product" key={product.slug}>
+            <Link to={`/products/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </Link>
+            <div className="product-info">
+              <Link to={`/products/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
+              <p>
+                <strong>$</strong> {product.price}
+              </p>
               <button>Add to cart</button>
-              </div>
             </div>
-          ))
-        }
+          </div>
+        ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
